122850e19bf971df5d7a6a7f9701be81ed2f3005
Christian Bell <christiantcbell@gmail.com>
2019-06-21 11:14:59 +0100

    added dotenv-rails for development

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,6 +1,8 @@
 require "shrine"
 require "shrine/storage/s3"
 
+p ENV['S3_BUCKET_MEDIA']
+
 s3_options = {
   access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
@@ -9,8 +11,8 @@ s3_options = {
 }
 
 Shrine.storages = {
-  cache: Shrine::Storage::S3.new(prefix: 'shrine-cache', public: true, **s3_options),
-  store: Shrine::Storage::S3.new(public: true, **s3_options),
+  cache: Shrine::Storage::S3.new(prefix: 'shrine-cache', **s3_options),
+  store: Shrine::Storage::S3.new(**s3_options),
 }
 
 Shrine.plugin :activerecord
