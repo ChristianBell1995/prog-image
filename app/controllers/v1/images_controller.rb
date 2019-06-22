0df90a2200504d71113b80a92183b981b3fa690e
Christian Bell <christiantcbell@gmail.com>
2019-06-22 12:39:10 +0100

    added user_id to filename path

--- app/controllers/v1/images_controller.rb
+++ app/controllers/v1/images_controller.rb
@@ -1,7 +1,7 @@
 class V1::ImagesController < ApplicationController
   def create
     return unless current_user.present?
-    @image = current_user.images.create(filename: SecureRandom.urlsafe_base64,
+    @image = current_user.images.create(filename: "#{current_user.id}/#{SecureRandom.urlsafe_base64}",
                                         image: create_image_params[:file])
 
     render json: @image.render_json, status: :created
