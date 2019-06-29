9a544b6a6fc4394bf896ac87df5cb43dd443daf6
Christian Bell <christiantcbell@gmail.com>
2019-06-29 11:24:05 +0100

    added background job work to tell if a job is still being processed

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ApplicationRecord
   include ImageUploader::Attachment.new(:image)
-  include Sidekiq::Worker
   include Sidekiq::Status::Worker
+  include Sidekiq::Worker
 
   belongs_to :user
 
@@ -9,7 +9,10 @@ class Image < ApplicationRecord
   IMAGE_MIME_TYPES = %w[image/jpg image/png image/jpeg image/gif image/tiff]
 
   STATUS_UPLOADED = 1
-  STATUS_PENDING_UPLOAD = 0
+  IMAGE_STATUS_MAPPING = {
+    0 => 'Still Processing...',
+    1 => 'Uploaded!'
+  }
 
   def render_json
     { tracking_id: id, user_id: user_id, message: 'Your image is being processed!' }
@@ -17,29 +20,41 @@ class Image < ApplicationRecord
 
   class << self
     def user_images_json(user_id)
-      images = where(user_id: user_id).order(:asc)
-      master_json = []
+      images = where(user_id: user_id).order(id: :desc)
+      all_images = []
       images.map do |image|
         image_json = {}
         image_json[:tracking_id] = image.id
         image_json[:file_url] = "#{ApplicationRecord::S3_PATH}#{image.filename}.png"
-        image_json[:status] = determine_status(image)
+        image_json[:status] = IMAGE_STATUS_MAPPING[determine_status(image)]
         image_json[:created_at] = image.created_at
-        master_json << image_json
+        all_images << image_json
       end
-      master_json
+      all_images
     end
 
     private
 
     def determine_status(image)
-      return 'Uploaded' if image.status == STATUS_UPLOADED
+      return image.status if image.status == STATUS_UPLOADED
+
       workers = Sidekiq::Workers.new
-      workers.each do |_process_id, _thread_id, work|
-        p work
-        # To do get the image ID from a worker to return status
+      image_worker = workers.select do |_process_id, _thread_id, work|
+        image_id = work.dig('payload', 'args', 0, 'arguments', 0, 'record', 1).to_i
+        image_id == image.id
       end
+      check_job_processing(image_worker, image)
+      image.status
     end
 
+    def check_job_processing(worker, image)
+      if image_worker.empty?
+        image.update(status: STATUS_UPLOADED)
+      else
+        job_id = worker.dig(0, 2, 'payload', 'jid')
+        job_status = Sidekiq::Status::get job_id, :status
+        image.update(status: STATUS_UPLOADED) if job_status == 'complete'
+      end
+    end
   end
 end
